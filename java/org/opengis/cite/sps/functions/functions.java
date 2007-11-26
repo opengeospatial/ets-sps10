b1ea96f554171e8e5ddedbb88156746a452a3cf7
sfairgrieve <sfairgrieve2003@gmail.com>
2007-11-26 21:49:54 +0000

    git-svn-id: https://svn.opengeospatial.org/ogc-projects/cite/trunk/sps/1.0/ETS@1985 32909982-3df7-0310-a142-94324ff2e7a9

--- java/org/opengis/cite/sps/functions/functions.java
+++ java/org/opengis/cite/sps/functions/functions.java
@@ -1,15 +1,27 @@
+/*
+ * functions.java
+ *
+ * Created on November 26, 2007, 11:53 AM
+ *
+ * This softare was created under contract with the US Government
+ */
 package org.opengis.cite.sps.functions;
 
 import java.util.Calendar;
 import java.util.Date;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-
+/**
+ *
+ * @author Northrop Grumman IT TASC
+ */
 public class functions {
+    
     public static String getCurrentDateTime() {
         Calendar cal = Calendar.getInstance();
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-ddTHH:mm:ss.fff");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
         Date currentDateTime = cal.getTime();
         return dateFormat.format(currentDateTime);
     }
+    
 }
